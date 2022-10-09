fa87da5bf131a1d1da843812f88192beece55c69
MartoMcfly <martinpelaez@hotmail.es>
2022-10-10 00:46:37 +0200

    Bug: Pantalla revarga en progreso

--- Project1/assignment102.py
+++ Project1/assignment102.py
@@ -21,3 +21,27 @@ for line in handle:
         count[str(line[-2])] = count.get(str(line[-2]),0) + 1
         
 print(count)
+
+x , y = 3, 4
+print(type(x))
+
+x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
+y = x.items()
+print( y)
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+count = dict()
+c = 0
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From '):
+        line = line.split()
+        c = c + 1
+        print(str(line[-2]))
+        count[str(line[-2])] = count.get(str(line[-2]),0) + 1
+        
+print(count)
