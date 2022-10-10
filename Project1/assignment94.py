8ad6eaf9bbe4c29c4b7cd75619864877d46a2d82
MartoMcfly <martinpelaez@hotmail.es>
2022-10-10 23:58:45 +0200

    Bug: Change on assignment

--- Project1/assignment94.py
+++ Project1/assignment94.py
@@ -37,3 +37,43 @@ for key in count:
         bigNumber = count[key]
         bigKey = key
 print(bigKey + ' ' + str(bigNumber))
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+count = dict()
+
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From '):
+        
+        count[line.split()[1]] = count.get(line.split()[1],0) + 1
+
+bigKey = 0
+bigNumber = 0
+for key in count:
+    if count[key] > bigNumber:
+        bigNumber = count[key]
+        bigKey = key
+print(bigKey + ' ' + str(bigNumber))
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+count = dict()
+
+for line in handle:
+    line = line.rstrip()
+    if line.startswith('From '):
+        
+        count[line.split()[1]] = count.get(line.split()[1],0) + 1
+
+bigKey = 0
+bigNumber = 0
+for key in count:
+    if count[key] > bigNumber:
+        bigNumber = count[key]
+        bigKey = key
+print(bigKey + ' ' + str(bigNumber))
