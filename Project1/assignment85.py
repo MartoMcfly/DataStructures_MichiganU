a853880fb6640ca5bbd705e76f93367e884491ba
MartoMcfly <martinpelaez@hotmail.es>
2022-11-11 15:55:05 -0500

    LeetCode exercises 2

--- Project1/assignment85.py
+++ Project1/assignment85.py
@@ -41,17 +41,3 @@ for line in fh:
         count = count + 1
 print("There were", count, "lines in the file with From as the first word")
 
-fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
-
-fh = open(fname)
-count = 0
-
-for line in fh:
-    line = line.strip()
-    if line.startswith('From '):
-        print(line.split()[1])
-        count = count + 1
-print("There were", count, "lines in the file with From as the first word")
-
